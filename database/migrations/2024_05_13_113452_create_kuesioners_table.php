e845c585ccc7db25bf636385cadc00d34e85c269
ardiaanf <ardiaanf@gmail.com>
2024-05-19 14:29:57 +0700

    menambahkan tabel

---
+++ database/migrations/2024_05_13_113452_create_kuesioners_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kuesioners', function (Blueprint $table) {
+            $table->id_kuesioner();
+            $table->integer('id_role');
+            $table->string('jenis_kuesioner', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kuesioners');
+    }
+};
---
+++ database/migrations/2024_05_13_134141_create_response_datas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('response_datas', function (Blueprint $table) {
+            $table->id_responseData();
+            $table->integer('id_user');
+            $table->integer('pertanyaan');
+            $table->integer('id_jawaban');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('response_datas');
+    }
+};
