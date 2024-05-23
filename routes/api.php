6c8c524bd7901ca3b91fc64144137c2a1cb1ca26
ardiaanf <ardiaanf@gmail.com>
2024-05-23 08:33:10 +0700

    penambahan routing autentikasi

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\DosenController;
 use App\Http\Controllers\API\KriteriaController;
 use App\Http\Controllers\API\KuesionerController;
@@ -8,6 +9,7 @@ use App\Http\Controllers\API\MonitoringController;
 use App\Http\Controllers\API\PenilaianController;
 use App\Http\Controllers\API\PertanyaanController;
 use App\Http\Controllers\API\RangkingController;
+use App\Http\Controllers\API\userController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,8 +24,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [userController::class, 'show']);
+    Route::put('/user', [userController::class, 'update']);
 });
 
 // Manajemen Kriteria Penilaian
