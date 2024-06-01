ac4c3976ef4419c2272f17c9e1d2050ea25124c3
fazaimron27 <cincaumyself@gmail.com>
2024-06-01 15:41:04 +0700

    add resources

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'reg_number' => $this->reg_number,
+            'study_program_id' => $this->study_program_id,
+            'email_verified_at' => $this->email_verified_at,
+            'roles' => $this->roles->pluck('name'),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
