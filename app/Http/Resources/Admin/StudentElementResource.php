d48605599a45b010993cdcaac6235fa191052c46
fazaimron27 <cincaumyself@gmail.com>
2024-06-12 13:12:37 +0700

    add student element resource

---
+++ app/Http/Resources/Admin/StudentElementResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentElementResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'student_questions' => StudentQuestionResource::collection($this->whenLoaded('studentQuestions')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
