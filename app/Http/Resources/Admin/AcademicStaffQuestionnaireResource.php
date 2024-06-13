b16685fd92336163fb85086da192dfcf1cc21e68
ardiaanf <ardiaanf@gmail.com>
2024-06-13 22:51:19 +0700

    add Resource academic staff

---
+++ app/Http/Resources/Admin/AcademicStaffQuestionnaireResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AcademicStaffQuestionnaireResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name'=> $this->name,
+            'description'=> $this->description,
+            'admin'=> $this->whenLoaded('admin', function() {
+                return $this->admin->name;
+            }),
+            'acastaff_elements' => AcademicStaffElementResource::collection($this->whenLoaded('acastaffElements')) ,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
