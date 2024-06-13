7f213083d101eac82d600b074b497211405e12fe
ardiaanf <ardiaanf@gmail.com>
2024-06-13 08:00:20 +0700

    add resource element,questinnaire,question lecturer

---
+++ app/Http/Resources/Admin/LecturerQuestionnaireResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\Admin;
+
+use App\Models\LecturerElement;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LecturerQuestionnaireResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name'=> $this->name,
+            'description'=> $this->description,
+            'type'=> $this->type,
+            'admin'=> $this->whenLoaded('admin', function() {
+                return $this->admin->name;
+            }),
+            'lecturer_elements' => LecturerElementResource::collection($this->whenLoaded('lecturerElements')) ,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
